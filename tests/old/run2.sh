4cf5c3a01d2eebe866906cf9c53d8954817ac4f0
dgiantsidi <dimitra.giantsidi@gmail.com>
2021-06-09 15:07:06 +0300

    cleanup

--- tests/old/run2.sh
+++
@@ -1,9 +0,0 @@
-#!/env/bin/bash
-
-# Test the secure_recv() and secure_sent() functions
-python ./tests/single_threaded_server.py &
-sleep 1
-python ./tests/single_threaded_client.py
-sleep 1
-
-echo "Finished tests .."
